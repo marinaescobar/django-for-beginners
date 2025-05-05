ea283f84d3d8d107a9853b0e4888e4e859e0d418
marinaespe <marina.escobar.perez@gmail.com>
2025-05-05 20:34:57 +0200

    Erase project

--- Blog/manage_post/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse #Devuelve una respuesta
-
-def manage_post(request):
-    return HttpResponse("¡Bienvenido a la página de Manage Post!")
